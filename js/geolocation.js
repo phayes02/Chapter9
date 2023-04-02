d5764ad8e0e9aff1eef81dda3ad56c2e69525b75
phayes02 <123522954+phayes02@users.noreply.github.com>
2023-04-02 13:45:45 -0500

    Add files via upload

---
+++ js/geolocation.js
@@ -0,0 +1,21 @@
+var elMap = document.getElementById('loc');                 
+var msg = 'Sorry, we were unable to get your location.';    
+
+if (Modernizr.geolocation) {                                
+  navigator.geolocation.getCurrentPosition(success, fail);  
+  elMap.textContent = 'Checking location...';               
+} else {                                                   
+  elMap.textContent = msg;                                  
+}
+
+function success(position) {                               
+  msg = '<h3>Longitude:<br>';                              
+  msg += position.coords.longitude + '</h3>';            
+  msg += position.coords.latitude + '</h3>';                
+  elMap.innerHTML = msg;                                  
+}
+
+function fail(msg) {                                       
+  elMap.textContent = msg;                                 
+  console.log(msg.code);                                    
+}
